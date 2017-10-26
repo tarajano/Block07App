c7c0241bcb3eb3370fa50573061d8024d383a4db
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-26 19:00:13 -0400

    SUMMARY: Ads NumberPicker obj and sets its displayed values.
    REMARKS: coursera.org/learn/android-app
    TODO: Associate actions to NumberPicker obj.
    
    FILE: MainActivity
      ADDS: NumberPicker obj, sets  its displayed values on pickerOptions.
      ADDS: declaration for navigate().

--- app/src/main/java/com/example/tarajano/block07app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block07app/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.tarajano.block07app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    NumberPicker picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        picker = (NumberPicker) findViewById(R.id.numberPicker);
+        String[] pickerOptions = {"Coursera", "Android", "Shopify", "Algonquin"};
+        picker.setDisplayedValues(pickerOptions);
+        picker.setMaxValue(pickerOptions.length - 1);
+        picker.setMinValue(0);
+    }
+
+    public void navigate(View view) {
     }
 }
