c293005acb924779c79e6951ac8d6996cfa8ad83
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-26 20:15:14 -0400

    SUMMARY: Ads a WebView, ads permision to access internet.
    REMARKS: coursera.org/learn/android-app
    TODO:
    
    FILE: AndroidManifest
      ADDS: Permission to access internet.
    
    FILE: MainActivity
      ADDS: Code to display web pages (WebViews).
    
    FILE: main_activity
      ADDS: WebView elements.

--- app/src/main/java/com/example/tarajano/block07app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block07app/MainActivity.java
@@ -3,24 +3,43 @@ package com.example.tarajano.block07app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
 
     NumberPicker picker;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        webView = (WebView) findViewById(R.id.webView);
+
         picker = (NumberPicker) findViewById(R.id.numberPicker);
-        String[] pickerOptions = {"Coursera", "Android", "Shopify", "Algonquin"};
+        String[] pickerOptions = {"Coursera", "Android Studio", "Shopify", "Algonquin"};
         picker.setDisplayedValues(pickerOptions);
         picker.setMaxValue(pickerOptions.length - 1);
         picker.setMinValue(0);
     }
 
     public void navigate(View view) {
+        int pickerChoice = picker.getValue();
+        webView.setWebViewClient(new WebViewClient());
+        if(pickerChoice == 0)
+            webView.getProgress();
+            webView.loadUrl("http://www.coursera.org");
+        else if(pickerChoice == 1)
+            webView.loadUrl("https://developer.android.com");
+        else if(pickerChoice == 2)
+            webView.loadUrl("https://www.shopify.com");
+        else if(pickerChoice == 3)
+            webView.loadUrl("https://www.algonquincollege.com");
+        else
+            webView.loadUrl("https://www.google.com");
+
     }
 }
